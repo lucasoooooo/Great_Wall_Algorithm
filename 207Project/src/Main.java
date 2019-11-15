dfbe402e75030dd621528b4d7a77b4eb8d5a21c3
Lucas Balangero <46410300+lucasoooooo@users.noreply.github.com>
2019-11-14 19:43:06 -0500

    Update Main.java

--- 207Project/src/Main.java
+++ 207Project/src/Main.java
@@ -1,9 +1,90 @@
+<<<<<<< HEAD
+/**
+ * 207 Traveling Salesman Project.
+ * 11/14/2019
+ * Great Wall Algorithm
+ * 
+ * 
+ * 
+ * @author Alex
+ * @author Lucas
+ * 
+ */
+//Create Junit Tests
+public class Main {
+
+	public static void main(String[] args) {
+		
+		/*In order:
+		Rockville, 
+		Silver Spring, 
+		Philadelphia, 
+		Pittsburgh, 
+		Baltimore, 
+		Cleveland, 
+		New York City 
+		*/
+		final int [][]CITIES_DISTANCES = {
+				{0, 13, 142, 225, 40, 352, 227},
+				{13, 0, 136, 237, 34, 363, 222},
+				{141, 135, 0, 305, 101, 432, 97},
+				{226, 237, 304, 0, 248, 133, 371},
+				{40, 34, 106, 248, 0, 374, 192},
+				{352, 364, 431, 133, 375, 0, 462},
+				{228, 222, 97, 370, 188, 462, 0},
+				};
+		
+		
+	}
+	
+	
+	
+	public static double hoursOnRoad(){
+		return 0;
+	}
+	
+	public static double fuelCost(){
+		return 0;
+	}
+	
+	public static double driversSalary(){
+		return 0;
+	}
+	
+	public static double helpersSalary(){
+		return 0;
+	}
+	
+	public static double hotelCost(){
+		return 0;
+	}
+	
+	public static double mealCost(){
+		return 0;
+	}
+	
+	public static double maintenanceCost(){
+		return 0;
+	}
+	
+	public static double totalCostTruck() {
+		return 0;
+	}
+	
+	public static int totalDistance(int[][] array) {
+		return 0;
+	}
+	
+	public static String cityNames() {
+		return null;
+=======
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+>>>>>>> branch 'master' of https://github.com/ajuan3/Great_Wall_Algorithm
 	}
 
 }
